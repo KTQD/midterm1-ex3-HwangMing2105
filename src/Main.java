9bdf33f9707e0a5c26289550f88ca4c7ff69e274
HwangMing2105 <157661893+HwangMing2105@users.noreply.github.com>
2024-03-14 08:25:27 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(input.txt))){
+        StringBuilder nd = new StringBuilder();
+        String line;
+        while((line = reader.readLine() != null){
+            nd.append(line).append("\n");
+        }
+        reader.close();
 
+        String VungTau = nd.toString().replaceAll("Nha Trang","Vũng Tàu");
 
-        Bài làm viết tại đây
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(output.txt))){
+            writer.write(VungTau);
+            writer.close();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    } catch (IOException e){
+        System.out.println(e.getMessage());
+        e.printStackTrace();
+    }
 
     }
 }
